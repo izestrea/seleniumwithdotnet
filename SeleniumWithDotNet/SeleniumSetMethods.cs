7f635873674f747cedf2d1e191cdc30c13f56cda
izestrea <izestrea@gmail.com>
2016-07-28 16:49:29 +0300

    Refactoring the code, to be more generic

--- SeleniumWithDotNet/SeleniumSetMethods.cs
+++ SeleniumWithDotNet/SeleniumSetMethods.cs
@@ -11,30 +11,21 @@ namespace SeleniumWithDotNet
     class SeleniumSetMethods
     {
         // enter text
-        public static void EnterText(string element, string value, PropertyType elementtype)
+        public static void EnterText(IWebElement element, string value)
         {
-            if (elementtype == PropertyType.Id)
-                PropertiesCollection.driver.FindElement(By.Id(element)).SendKeys(value);
-            if (elementtype == PropertyType.Name)
-                PropertiesCollection.driver.FindElement(By.Name(element)).SendKeys(value);
+            element.SendKeys(value);
         }
 
         // click into a button, checkbox, option, etc.
-        public static void Click(string element, PropertyType elementtype)
+        public static void Click(IWebElement element)
         {
-            if (elementtype == PropertyType.Id)
-                PropertiesCollection.driver.FindElement(By.Id(element)).Click();
-            if (elementtype == PropertyType.Name)
-                PropertiesCollection.driver.FindElement(By.Name(element)).Click();
+            element.Click();
         }
 
         // selecting a dropdown control
-        public static void SelectDropDown(string element, string value, PropertyType elementtype)
+        public static void SelectDropDown(IWebElement element, string value)
         {
-            if (elementtype == PropertyType.Id)
-                new SelectElement(PropertiesCollection.driver.FindElement(By.Id(element))).SelectByText(value);
-            if (elementtype == PropertyType.Name)
-                new SelectElement(PropertiesCollection.driver.FindElement(By.Name(element))).SelectByText(value);
+            new SelectElement(element).SelectByText(value);
         }
     }
 }
